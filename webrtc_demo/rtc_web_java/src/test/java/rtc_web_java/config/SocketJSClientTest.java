ab1f59b982a169bf221d0920e27b7c64b49b7f42
m <m@m-PC>
2018-04-27 16:57:57 +0800

    ...

--- webrtc_demo/rtc_web_java/src/test/java/rtc_web_java/config/SocketJSClientTest.java
+++ webrtc_demo/rtc_web_java/src/test/java/rtc_web_java/config/SocketJSClientTest.java
@@ -5,16 +5,25 @@ import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.concurrent.FailureCallback;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.SuccessCallback;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-import org.springframework.web.socket.sockjs.transport.SockJsServiceConfig;
-import org.springframework.web.socket.sockjs.transport.handler.DefaultSockJsService;
-import org.springframework.web.socket.sockjs.transport.handler.SockJsWebSocketHandler;
 
+
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {RootConfig.class,WebConfig.class})
 public class SocketJSClientTest {
 
 	
@@ -26,11 +35,61 @@ public class SocketJSClientTest {
 		SockJsClient client=new SockJsClient(Arrays.asList(
 				new WebSocketTransport(new StandardWebSocketClient()))) ;
 		
-		SockJsServiceConfig config=new DefaultSockJsService
+		//SockJsServiceConfig config=new DefaultSockJsService
 		
-		client.doHandshake(new SockJsWebSocketHandler(serviceConfig, webSocketHandler, sockJsSession), uriTemplate, uriVars);
+		ListenableFuture<WebSocketSession> future=client.doHandshake(new WebSocketHandler() {
+			
+			@Override
+			public boolean supportsPartialMessages() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+				// TODO Auto-generated method stub
+				System.out.println("message:"+message);
+			}
+			
+			@Override
+			public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+				// TODO Auto-generated method stub
+				
+			}
+		},"http://localhost:8080/rtc_web/test");
+		
+		future.addCallback(new SuccessCallback<Object>() {
+
+			@Override
+			public void onSuccess(Object result) {
+				// TODO Auto-generated method stub
+				System.out.println("success");
+				
+			}
+		}, new FailureCallback() {
+			
+			@Override
+			public void onFailure(Throwable ex) {
+				// TODO Auto-generated method stub
+				System.out.print("fail");
+			}
+		});
 		
 	}
+	
+	
 	
 	
 	
